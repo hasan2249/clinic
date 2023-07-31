1e99bd424124be2d0f96a70c5781879bf1be3175
Hasan Jahjah <hasanjahjah224@gmail.com>
2023-07-31 13:31:51 +0300

    run app

--- application.bat
+++ application.bat
@@ -1,11 +1,11 @@
 @echo off
 
 :: open xammp server (apatch & mysql)
-cd "C:\xampp"
+cd "C:\xampp7.4"
 start xampp_start.EXE
 
 :: server php by artisan 
-cd "C:\xampp\htdocs\moneyTransfer"
+cd "C:\xampp7.4\htdocs\clinic"
 start "" php artisan serve --host=0.0.0.0
 
 :: wait 5 seconds to ensure that servers were opened
@@ -13,4 +13,4 @@ ping 127.0.0.1 -n 3 > nul
 
 :: open brawser to localhost
 ::start "" http://127.0.0.1:8000
-start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome_proxy.exe"  --profile-directory=Default --app-id=mngloiodpbedloingimookgdhhkgcblo
+start "" "C:\Program Files\Google\Chrome\Application\chrome_proxy.exe" --profile-directory=Default --app-id=nmaibjeohknfbkhgkhhkpphdopehbafp
